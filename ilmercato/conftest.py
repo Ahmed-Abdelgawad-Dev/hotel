9a5618fece734a63c75bcf08efb6fb037b6dc68c
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2025-09-21 22:07:55 +0300

    First Initial Code

---
+++ ilmercato/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ilmercato.users.models import User
+from ilmercato.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
